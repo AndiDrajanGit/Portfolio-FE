aed2ed9d8e3a5ed7d3764cc7a8a44163ed071cb7
Andi <andi.drajan@gmail.com>
2018-12-17 16:11:01 +0200

    carousel progress

--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -2,10 +2,16 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import Loader from 'components/Loader';
+import enhanceWithClickOutside from 'react-click-outside';
+import Carousel from 'nuka-carousel';
 
 import './ProjectDetails.scss';
 
 class ProjectDetails extends PureComponent {
+  handleClickOutside() {
+    this.props.hideDetails();
+  }
+
   render() {
     const { isLoaded, details, hideDetails } = this.props;
     console.log('DDDDDDD', isLoaded, details);
@@ -14,10 +20,32 @@ class ProjectDetails extends PureComponent {
         <Loader isVisible={!isLoaded} />
         {isLoaded && (
           <div className="project-details__content">
-            <div
-              className="project-details__images"
+            <Carousel
+              className="project-details__carousel"
               style={{ backgroundImage: `url(${details.cover_image})` }}
-            />
+              wrapAround
+              speed={750}
+              easing="easeCubicInOut"
+            >
+              {details.images.map((image, index) => (
+                <div key={index} className="project-details__carousel__item">
+                  {image.desktop && (
+                    <img
+                      className="project-details__carousel__item__desktop"
+                      src={image.desktop.url}
+                      alt={`${details.title} - ${index} - Desktop`}
+                    />
+                  )}
+                  {image.mobile && (
+                    <img
+                      className="project-details__carousel__item__mobile"
+                      src={image.mobile.url}
+                      alt={`${details.title} - ${index} - Mobile`}
+                    />
+                  )}
+                </div>
+              ))}
+            </Carousel>
           </div>
         )}
         <button onClick={hideDetails} className="font-bold project-details__close">
@@ -34,4 +62,4 @@ ProjectDetails.propTypes = {
   hideDetails: PropTypes.func.isRequired
 };
 
-export default ProjectDetails;
+export default enhanceWithClickOutside(ProjectDetails);
